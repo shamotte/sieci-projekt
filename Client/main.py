9b67d789d00d7612cbf73a63e4e8120b5fe65f0c
shamotte <123325697+shamotte@users.noreply.github.com>
2025-02-26 22:06:55 +0100

    Revert "Update main.py"
    This reverts commit 8d171cb7d018980333ff16a6bfb494aeec4abad8.

--- Client/main.py
+++ Client/main.py
@@ -7,29 +7,17 @@ import json
 
 
 def create_connection(ip_adres, port, client_id):
-    context = ssl.create_default_context()
-    context.check_hostname = False
-    context.verify_mode = ssl.CERT_NONE
-
     write_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    wrapped_write_socket = context.wrap_socket(write_socket,server_hostname=ip_adres)
+    write_socket.connect((ip_adres, port))
+    write_socket.send(str.encode(client_id))
 
-
-    wrapped_write_socket.connect((ip_adres, port))
-    wrapped_write_socket.send(str.encode(client_id))
-
-    message = wrapped_write_socket.recv(10);
+    message = write_socket.recv(10);
 
     read_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-
-    wrapped_read_socket = context.wrap_socket(read_socket,server_hostname=ip_adres)
-
-
-    wrapped_read_socket.connect((ip_adres, port))
-    wrapped_read_socket.send(str.encode(client_id))
-    message = wrapped_read_socket.recv(10)
-    return (wrapped_write_socket, wrapped_read_socket)
+    read_socket.connect((ip_adres, port))
+    read_socket.send(str.encode(client_id))
+    message = read_socket.recv(10)
+    return (write_socket, read_socket)
 
 
 def send_message(socket, recepiepient, message, client_id):
